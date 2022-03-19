bd2949fe3d4f8be14b5b4966e854ce145d1f87aa
s24729-pj <s24729@pjwstk.edu.pl>
2022-03-19 16:22:54 +0100

    kolokwium2

---
+++ kolokwium2/kolokwium2/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] tablica = {"1","6","1"};
+        System.out.println(sum(tablica));
+        System.out.println(count(tablica, 1));
+
+        public static int sum(tablica) {
+        for(int i = 0; i < tablica.length; i++) {
+            int wynik = 0;
+            wynik = wynik + tablica[i];
+            return wynik;
+        }
+        }
+
+        public static int count(String[] array, int counted) {
+           /*for (int i = 0; i < tablica.length; i++)
+            {
+
+            } */
+            return -1;
+        }
+    }
+}
